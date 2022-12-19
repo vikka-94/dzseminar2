743d25d98cdd2432bd3ad17624a6760146d1b41c
Vika <vikka-94@mail.ru>
2022-12-19 18:08:29 +0300

    dz10

---
+++ dz10/Program.cs
@@ -0,0 +1,28 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+int number = ReadInt("Введите трехзначное число: ");
+int sum = number.ToString().Length;
+
+if (sum < 3 && sum > 3)
+{
+    Console.WriteLine("Это не трехзначное число");
+}
+else
+{
+    Console.WriteLine(InCenter(number));
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int InCenter(int a)
+{
+    int result = ((a / 10) % 10);
+    return result;
+}
